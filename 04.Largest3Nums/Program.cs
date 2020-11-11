2450606c7165904fd0092ca0a4eed6f3292326df
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-11 20:45:17 +0200

    Add project files.

---
+++ 04.Largest3Nums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.Largest3Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Associative-Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace Associative_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, List<string>> dictionary = new SortedDictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word].Add(synonym);
+                }
+                else
+                {
+                    dictionary.Add(word, new List<string>());
+                    dictionary[word].Add(synonym);
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        } 
+    }
+}
