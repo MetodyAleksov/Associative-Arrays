b2b97dabb0ead7ad3da97a18038bb2065d82cbfc
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-13 16:49:21 +0200

    updated

--- 08.CompnayUsers/Program.cs
+++ 08.CompnayUsers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08.CompnayUsers
 {
@@ -6,7 +8,39 @@ namespace _08.CompnayUsers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, List<string>> companyRoster = new Dictionary<string, List<string>>();
+            string[] input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            while (input[0] != "End")
+            {
+                string company = input[0];
+                string employee = input[1];
+                if (companyRoster.ContainsKey(company))
+                {
+                    if (!companyRoster[company].Contains(employee))
+                    {
+                        companyRoster[company].Add(employee);
+                    }
+                }
+                else
+                {
+                    companyRoster.Add(company, new List<string>() { employee });
+                }
+                input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+
+            foreach (var item in companyRoster.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+                foreach (string employee in item.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
         }
     }
 }
